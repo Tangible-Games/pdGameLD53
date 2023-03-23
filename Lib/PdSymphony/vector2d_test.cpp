cab0cffbe04db1a880e49ea046611ed59ec3a11a
Dmitrii Tokarev <truvor.skameikin@gmail.com>
2023-03-23 22:25:33 +0100

    Fixes test

--- Lib/PdSymphony/vector2d_test.cpp
+++ Lib/PdSymphony/vector2d_test.cpp
@@ -7,7 +7,7 @@ void vector2d_sum() {
   PdSymphony::Math::Vector2d v2(5.0f, 7.0f);
   PdSymphony::Math::Vector2d v3 = v1 + v2;
   TEST_ASSERT_EQ(6.0f, v3.x);
-  TEST_ASSERT_EQ(19.0f, v3.y);
+  TEST_ASSERT_EQ(9.0f, v3.y);
 }
 
 int main() {
