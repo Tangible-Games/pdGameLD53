cec9d893469d35019efe6ccea27f443113e76936
Ivan Loskutov <loskutov.ivan@gmail.com>
2023-04-06 18:39:23 +0200

    Switch tests to google test
    Build dir should be reconfigured:
    $ meson setup builddir --reconfigure

--- Lib/PdSymphony/segment2d_test.cpp
+++ Lib/PdSymphony/segment2d_test.cpp
@@ -1,8 +1,8 @@
 #include "segment2d.hpp"
 
-#include "better_assert.hpp"
+#include <gtest/gtest.h>
 
-void Segment2d_intersect() {
+TEST(Segment2d, Intersect) {
   {
     PdSymphony::Math::Segment2d seg0(PdSymphony::Math::Point2d(0.0f, 0.0f),
                                      PdSymphony::Math::Point2d(0.0f, 2.0f));
@@ -10,13 +10,8 @@ void Segment2d_intersect() {
                                      PdSymphony::Math::Point2d(1.0f, 1.0f));
     PdSymphony::Math::Point2d p;
     bool result = seg0.Intersect(seg1, 0.00001f, p);
-    TEST_ASSERT_EQ(true, result);
-    TEST_ASSERT_EQ(0.0f, p.x);
-    TEST_ASSERT_EQ(1.0f, p.y);
+    ASSERT_EQ(true, result);
+    ASSERT_EQ(0.0f, p.x);
+    ASSERT_EQ(1.0f, p.y);
   }
-}
-
-int main() {
-  Segment2d_intersect();
-  return 0;
 }
