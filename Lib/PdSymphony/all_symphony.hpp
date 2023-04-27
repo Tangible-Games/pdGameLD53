ee35d318c731ed509b2090dd0e855f10ef527e3a
Dmitrii Tokarev <truvor.skameikin@gmail.com>
2023-04-27 18:02:13 +0200

    Adds missing file

---
+++ Lib/PdSymphony/all_symphony.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "aa_rect2d.hpp"
+#include "angle.hpp"
+#include "point2d.hpp"
+#include "point3d.hpp"
+#include "ray_casting_projection.hpp"
+#include "segment2d.hpp"
+#include "transformation_matrix3d.hpp"
+#include "vector2d.hpp"
+#include "vector3d.hpp"
