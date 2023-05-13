edf060481f3d628be991d4897fe79000cdf4d1da
Dmitrii Tokarev <truvor.skameikin@gmail.com>
2023-05-13 23:13:43 +0200

    Formats code

--- Lib/PdSymphony/pd_animations_test.cpp
+++ Lib/PdSymphony/pd_animations_test.cpp
@@ -8,14 +8,11 @@ static playdate_graphics graphics;
 static PlaydateAPI playdate;
 
 static LCDBitmapTable* bitmap_table = reinterpret_cast<LCDBitmapTable*>(1);
-static const int bitmap_table_num_bitmaps = 6; // 5 frames + 1 for nullptr
+static const int bitmap_table_num_bitmaps = 6;  // 5 frames + 1 for nullptr
 static LCDBitmap* bitmap_table_bitmaps[bitmap_table_num_bitmaps] = {
-  reinterpret_cast<LCDBitmap*>(1),
-  reinterpret_cast<LCDBitmap*>(2),
-  reinterpret_cast<LCDBitmap*>(3),
-  reinterpret_cast<LCDBitmap*>(4),
-  reinterpret_cast<LCDBitmap*>(5),
-  0,
+    reinterpret_cast<LCDBitmap*>(1), reinterpret_cast<LCDBitmap*>(2),
+    reinterpret_cast<LCDBitmap*>(3), reinterpret_cast<LCDBitmap*>(4),
+    reinterpret_cast<LCDBitmap*>(5), 0,
 };
 
 extern "C" {
