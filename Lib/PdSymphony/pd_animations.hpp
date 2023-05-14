bcaee3ac6114b0bc21cbcaec2158b2c795ec1ce3
Dmitrii Tokarev <truvor.skameikin@gmail.com>
2023-05-14 17:58:51 +0200

    Adds play not looped method to PdAnimation and test

--- Lib/PdSymphony/pd_animations.hpp
+++ Lib/PdSymphony/pd_animations.hpp
@@ -23,13 +23,52 @@ class PdAnimation {
     if (num_frames_ == -1) {
       updateNumFrames();
     }
+
+    is_playing_ = false;
+    looped_ = false;
+    running_time_ = 0.0f;
   }
 
   int GetNumFrames() const { return num_frames_; }
 
   float GetFps() const { return fps_; }
 
-  float GetLength() const { return fps_ * num_frames_; }
+  float GetLength() const { return ((float)num_frames_) / fps_; }
+
+  void Play(bool looped, bool from_start = true) {
+    is_playing_ = true;
+    looped_ = looped;
+
+    if (from_start) {
+      running_time_ = 0.0f;
+    }
+  }
+
+  void Stop(bool jump_to_start = true) {
+    is_playing_ = false;
+
+    if (jump_to_start) {
+      running_time_ = 0.0f;
+    }
+  }
+
+  void Update(float dt) {
+    if (!is_playing_) {
+      return;
+    }
+
+    running_time_ += dt;
+    if (!looped_) {
+      if (running_time_ > GetLength()) {
+        running_time_ = GetLength();
+      }
+    }
+  }
+
+  LCDBitmap* GetBitmap() {
+    return playdate_->graphics->getTableBitmap(bitmap_table_,
+                                               getCurrentFrame());
+  }
 
  private:
   void updateNumFrames() {
@@ -43,10 +82,26 @@ class PdAnimation {
     }
   }
 
+  int getCurrentFrame() const {
+    float frame_length = 1.0f / fps_;
+    int frame_index = 0;
+    if (looped_) {
+    } else {
+      frame_index = floorf(running_time_ / frame_length);
+      if (frame_index >= num_frames_) {
+        frame_index = num_frames_ - 1;
+      }
+    }
+    return frame_index;
+  }
+
   PlaydateAPI* playdate_{nullptr};
   LCDBitmapTable* bitmap_table_{nullptr};
   int num_frames_{-1};
   float fps_{1.0f};
+  bool is_playing_{false};
+  bool looped_{false};
+  float running_time_{0.0f};
 };
 }  // namespace Engine
 }  // namespace PdSymphony
--- Lib/PdSymphony/pd_animations_test.cpp
+++ Lib/PdSymphony/pd_animations_test.cpp
@@ -8,16 +8,23 @@ static playdate_graphics graphics;
 static PlaydateAPI playdate;
 
 static LCDBitmapTable* bitmap_table = reinterpret_cast<LCDBitmapTable*>(1);
-static const int bitmap_table_num_bitmaps = 6;  // 5 frames + 1 for nullptr
+static const int bitmap_table_num_bitmaps = 5;  // 5 frames + 1 for nullptr
 static LCDBitmap* bitmap_table_bitmaps[bitmap_table_num_bitmaps] = {
-    reinterpret_cast<LCDBitmap*>(1), reinterpret_cast<LCDBitmap*>(2),
-    reinterpret_cast<LCDBitmap*>(3), reinterpret_cast<LCDBitmap*>(4),
-    reinterpret_cast<LCDBitmap*>(5), 0,
+    // clang-format off
+  reinterpret_cast<LCDBitmap*>(1),
+  reinterpret_cast<LCDBitmap*>(2),
+  reinterpret_cast<LCDBitmap*>(3),
+  reinterpret_cast<LCDBitmap*>(4),
+  reinterpret_cast<LCDBitmap*>(5)
+    // clang-format on
 };
 
 extern "C" {
 LCDBitmap* getTableBitmap(LCDBitmapTable* table, int idx) {
   if (table == bitmap_table) {
+    if (idx < 0 || idx >= bitmap_table_num_bitmaps) {
+      return 0;
+    }
     return bitmap_table_bitmaps[idx];
   }
   return 0;
@@ -28,7 +35,40 @@ TEST(PdAnimation, CalculatesNumFrames) {
   graphics.getTableBitmap = getTableBitmap;
   playdate.graphics = &graphics;
 
-  PdAnimation animation(&playdate, bitmap_table, /* fps= */ 1.0f);
+  PdAnimation animation(&playdate, bitmap_table, /* fps= */ 10.0f);
 
   ASSERT_EQ(5, animation.GetNumFrames());
+  ASSERT_NEAR(0.5f, animation.GetLength(), 0.0001f);
+}
+
+TEST(PdAnimation, PlaysNotLooped) {
+  graphics.getTableBitmap = getTableBitmap;
+  playdate.graphics = &graphics;
+
+  PdAnimation animation(&playdate, bitmap_table, /* fps= */ 50.0f);
+
+  //    0 - 0.02 -> 1
+  // 0.02 - 0.04 -> 2
+  // 0.04 - 0.06 -> 3
+  // 0.06 - 0.08 -> 4
+  // 0.08 - 0.10 -> 5
+
+  animation.Play(/* looped= */ false);
+
+  ASSERT_EQ(reinterpret_cast<LCDBitmap*>(1), animation.GetBitmap());
+
+  animation.Update(0.019f);  // 0.019f
+  ASSERT_EQ(reinterpret_cast<LCDBitmap*>(1), animation.GetBitmap());
+
+  animation.Update(0.002f);  // 0.021f
+  ASSERT_EQ(reinterpret_cast<LCDBitmap*>(2), animation.GetBitmap());
+
+  animation.Update(0.04f);  // 0.061f
+  ASSERT_EQ(reinterpret_cast<LCDBitmap*>(4), animation.GetBitmap());
+
+  animation.Update(0.02f);  // 0.081f
+  ASSERT_EQ(reinterpret_cast<LCDBitmap*>(5), animation.GetBitmap());
+
+  animation.Update(0.1f);  // 0.181f
+  ASSERT_EQ(reinterpret_cast<LCDBitmap*>(5), animation.GetBitmap());
 }
