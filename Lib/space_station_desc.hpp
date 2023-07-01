494d6b565e9ab605d6de5aa28afad722299be8c4
Dmitrii Tokarev <truvor.skameikin@gmail.com>
2023-07-01 15:32:09 +0200

    Adds script that can convert station desc from json to c++

---
+++ Lib/space_station_desc.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <stdint.h>
+
+#include <array>
+#include <string>
+
+#include "PdSymphony/point2d.hpp"
+
+using namespace PdSymphony::Math;
+
+struct AsteroidPositionDesc {
+  Point2d pos;
+  float radius{0.0f};
+};
+
+struct SpaceStationDesc {
+  Point2d pos;  // Local to station.
+  std::string name;
+  std::string desc;
+  float jump_distance{0.0f};
+  std::string difficulty_str;
+  int difficulty{0};
+  std::vector<AsteroidPositionDesc> asteroids;
+};
