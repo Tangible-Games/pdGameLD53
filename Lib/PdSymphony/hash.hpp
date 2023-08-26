9f9a57402d9f7b152edb172bc85375735b569db4
Dmitrii Tokarev <truvor.skameikin@gmail.com>
2023-08-26 14:05:00 +0200

    Format

--- Lib/PdSymphony/hash.hpp
+++ Lib/PdSymphony/hash.hpp
@@ -5,12 +5,12 @@
 namespace PdSymphony {
 namespace Hash {
 inline uint32_t HashLy(const unsigned char* str, size_t length,
-                         uint32_t hash_start = 0) {
-    uint32_t result = hash_start;
-    for (size_t i = 0; i < length; ++i) {
-      result = (result * 1664525) + (*str) + 1013904223;
-    }
-    return result;
+                       uint32_t hash_start = 0) {
+  uint32_t result = hash_start;
+  for (size_t i = 0; i < length; ++i) {
+    result = (result * 1664525) + (*str) + 1013904223;
   }
+  return result;
+}
 }  // namespace Hash
 }  // namespace PdSymphony
